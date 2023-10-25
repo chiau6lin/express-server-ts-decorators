79d85476032d24c600863efe6610e062250d9fb1
Jamie Lin <jamielin0.tw@gmail.com>
2023-10-25 11:07:20 +0800

    refactor: add http method defined

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,7 +1,19 @@
 import "reflect-metadata";
+import { Methods } from "./methods";
 
-export function get(path: string) {
-  return function(target: any, key: string, descriptor: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function routeBinder(method: string) {
+  // factory decorator
+  return function(path: string) {
+    return function(target: any, key: string, descriptor: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    }
   }
 }
+
+
+export const get = routeBinder(Methods.get);
+export const post = routeBinder(Methods.post);
+export const put = routeBinder(Methods.put);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
